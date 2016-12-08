df02e1cd1995a3300405e2a94b14a167ac535a8b
cel54521 <pi3.14159265358979323846264333@gmail.com>
2016-12-09 00:06:56 +0900

    gr-peach program

---
+++ led_hearts/led_heart.cpp
@@ -0,0 +1,39 @@
+#include <kernel.h>
+#include <t_syslog.h>
+#include <t_stdlib.h>
+#include "syssvc/serial.h"
+#include "syssvc/syslog.h"
+#include "kernel_cfg.h"
+#include "arduino_app.h"
+#include "arduino_main.h"
+/* GR-PEACH Sketch Template V1.00 */
+#include <Arduino.h>
+#define INTERVAL 100
+int value1;
+int value2;
+int value3;
+void setup()
+{
+    value1 = 0;
+    value2 = 0;
+    value3 = 0;
+    pinMode(PIN_LED_RED   , OUTPUT);
+    pinMode(PIN_LED_GREEN , OUTPUT);
+    pinMode(PIN_LED_BLUE  , OUTPUT);
+    pinMode(PIN_LED_USER  , OUTPUT);
+    pinMode(PIN_SW        , INPUT);
+}
+void cyclic_handler(intptr_t exinf) {
+	irot_rdq(LOOP_PRI); /* change the running loop. */
+}
+void loop()
+{
+    digitalWrite(PIN_LED_BLUE, 1);
+end0:
+    ;
+}
+void loop1()
+{
+end1:
+    ;
+}
